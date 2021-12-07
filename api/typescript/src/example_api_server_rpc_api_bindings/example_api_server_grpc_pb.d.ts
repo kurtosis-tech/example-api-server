e3c664ff31b87be53837cf1ee3d247ef0889760b
mieubrisse <mieubrisse@gmail.com>
2021-12-06 20:03:28 -0600

    Switch to grpc-js instead of grpc for TS API lib

--- api/typescript/src/example_api_server_rpc_api_bindings/example_api_server_grpc_pb.d.ts
+++ api/typescript/src/example_api_server_rpc_api_bindings/example_api_server_grpc_pb.d.ts
@@ -5,7 +5,7 @@
 
 import * as example_api_server_pb from "./example_api_server_pb";
 import * as google_protobuf_empty_pb from "google-protobuf/google/protobuf/empty_pb";
-import * as grpc from "grpc";
+import * as grpc from "@grpc/grpc-js";
 
 interface IExampleAPIServerServiceService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
   isAvailable: grpc.MethodDefinition<google_protobuf_empty_pb.Empty, google_protobuf_empty_pb.Empty>;
--- api/typescript/src/example_api_server_rpc_api_bindings/example_api_server_grpc_pb.js
+++ api/typescript/src/example_api_server_rpc_api_bindings/example_api_server_grpc_pb.js
@@ -1,7 +1,7 @@
 // GENERATED CODE -- DO NOT EDIT!
 
 'use strict';
-var grpc = require('grpc');
+var grpc = require('@grpc/grpc-js');
 var example_api_server_pb = require('./example_api_server_pb.js');
 var google_protobuf_empty_pb = require('google-protobuf/google/protobuf/empty_pb.js');
 
